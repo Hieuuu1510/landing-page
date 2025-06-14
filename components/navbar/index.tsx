d8fa82c086e18e6d5f56a5887552019b1218d49c
TranMinhHieu <hieuhienlanh03@gmail.com>
2025-06-15 00:03:57 +0700

    feat: udpate navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
+  const navbarRef = useRef<HTMLDivElement>(null);
+  const [scrollEnd, setScrollEnd] = useState<boolean>(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setScrollEnd(!entry.isIntersecting);
+      },
+      {
+        root: null, // viewport
+        threshold: 0,
+      }
+    );
+
+    const el = navbarRef.current;
+    if (el) observer.observe(el);
+
+    return () => {
+      if (el) observer.unobserve(el);
+    };
+  }, []);
+
   return (
-    <header className="w-full h-[76px] bg-[var(--backgroundHeader)] text-white p-[10px]">
-      <div className="flex items-center justify-between w-[1410px] my-0 mx-auto">
-        <div className="uppercase cursor-pointer font-black text-[26px] px-[10px]">
-          Cozy Stay
-        </div>
-        <>
-          <ul className="flex gap-4">
+    <>
+      <div
+        ref={navbarRef}
+        className="h-[1px] bg-[var(--backgroundHeader)]"
+      ></div>
+      <div
+        className={`w-full h-auto bg-[var(--backgroundHeader)] text-white p-[10px] ${
+          scrollEnd ? "navbar" : ""
+        }`}
+      >
+        <div className="flex items-center justify-between max-w-[1410px] my-0 mx-auto md:max-w-[1210px]">
+          <div className="uppercase cursor-pointer font-black text-[26px] px-[10px]">
+            Cozy Stay
+          </div>
+          <ul className="hidden lg:flex">
             <li className="cursor-pointer my-[18px] mx-[15px]">
               <a href="/">Trang chủ</a>
             </li>
@@ -28,12 +58,12 @@ const Navbar = () => {
               <a href="">Liên hệ</a>
             </li>
           </ul>
-        </>
-        <div className="cursor-pointer px-[20px] py-[10px] bg-[var(--shop-color-main)] tracking-[1px] rounded-[22px]">
-          Đặt phòng
+          <div className="cursor-pointer px-[20px] py-[10px] bg-[var(--shop-color-main)] tracking-[1px] rounded-[22px]">
+            Đặt phòng
+          </div>
         </div>
       </div>
-    </header>
+    </>
   );
 };
 
